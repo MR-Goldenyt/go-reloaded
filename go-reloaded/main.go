e609441b71842ee68fb475fdbb1816f55ba6a847
ymuhamma <yousifmuhammad090@gmail.com>
2025-06-14 23:12:43 +0300

    added the local updated project

--- go-reloaded/main.go
+++ go-reloaded/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"piscine"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) > 3 || len(os.Args) < 3  {
-		fmt.Println("Error: Incorrect number of arguments. Expected [2], but received ["+strconv.Itoa(len(os.Args)-1)+"].\nPlease check the usage instructions.")
-		return		
+	if len(os.Args) > 3 || len(os.Args) < 3 {
+		fmt.Println("Error: Incorrect number of arguments. Expected [2], but received [" + strconv.Itoa(len(os.Args)-1) + "].\nPlease check the usage instructions.")
+		return
 	}
 	InputFile := os.Args[1]
 	OutputFile := os.Args[2]
